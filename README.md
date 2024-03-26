0c8236eaad5e625f319ad898b637353997b3a519
Bernie Gray <bfgray3@gmail.com>
2024-03-26 19:58:14 -0400

    add resample() function

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ i'm thinking of writing a package for bootstrapping. here are some of the curren
 * https://cran.r-project.org/web/packages/bootstrap/index.html
 * https://cran.r-project.org/web/packages/rsample/index.html
 * https://github.com/facebookarchive/bootstrapped
+
+at first i'll write it in c++ with no dependencies outside the standard library. i might add a python wrapper later.
+
+this repository should be considered in an unusable state as of now and full of scratchwork.
--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <random>
 #include <vector>
 
-int main() {
+auto resample(const auto& x) {
+  // adapted from https://stackoverflow.com/questions/33802205/how-to-sample-without-replacement-using-c-uniform-int-distribution
+  std::default_random_engine generator;
+  std::uniform_int_distribution<> distribution(0, x.size() - 1);
 
-  std::vector<int> x(100);
+  std::vector<int> replicate(x.size()); // TODO: get type from x
+  std::generate_n(
+    std::begin(replicate),
+    replicate.size(),
+    // TODO: make actually random
+    [&x, &distribution, generator = std::default_random_engine{}]() mutable {
+      return x[distribution(generator)];
+    }
+  );
+  return replicate;
+}
+
+int main() {
+  std::vector<int> x(10);
   std::iota(std::begin(x), std::end(x), 0);
 
+  for (const auto& e: resample(x)) std::cout << e << '\n';
 }
